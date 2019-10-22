95f1da4442d4a6f42692175c32ace5249a1d56fb
Matyáš Skalický <matyaskalicky@gmail.com>
2019-10-22 14:07:07 +0100

    Updated the graphing in the project part 1

--- part1/Task1.m
+++ part1/Task1.m
@@ -1,26 +1,28 @@
 %declaring variables & initialization
-for i=1:1
-    lambda= 10^(-3+i);
+for i = 1:1
+    lambda = 10^(-3+i);
     T = 80;
-    U_max = 100;
-    p_initial = [0 ; 5];
-    p_final = [15 ; -15];
-    w = [10 20 30 30 20 10;
-        10 10 10 0  0 -10 ];
     t = T;
-    tau = [10 25 30 40 50 60];
-    A = [1 0 0.1 0;
-         0 1 0 0.1;
-         0 0 0.9 0;
-         0 0 0   0.9];
-    B = [0   0;
-         0   0;
-         0.1 0;
-         0   0.1];
+    U_max = 100;
     
-    % ITS THE ONLY WAY, otherwise matrix calculations wouldnt be possible
+    p_initial = [ 0;   5];
+    p_final   = [15; -15];
+    
+    w = [10 20 30 30 20 10;
+         10 10 10 0  0 -10 ];
+    
+    tau = [10 25 30 40 50 60];
+    
+    A = [1.0 0.0 0.1 0.0;
+         0.0 1.0 0.0 0.1;
+         0.0 0.0 0.9 0.0;
+         0.0 0.0 0.0 0.9];
+    B = [0.0 0.0;
+         0.0 0.0;
+         0.1 0.0;
+         0.0 0.1];
     E = [1 0 0 0 ; 
-        0 1 0 0];
+         0 1 0 0];
 
     cvx_begin
         variable x(4,t);
@@ -36,14 +38,29 @@ for i=1:1
             norm(u(:,1:t-1)) <= U_max
     cvx_end
     
-    hold on
-    figure();
-    scatter(w(1, :), w(2, :));
-    plot(x(1, :),x(2, :));
-    
+    % Plot the positions of the robot
+    ax = axes('Parent',figure);
+    hold(ax,'on');
+    % The initial and end position
+    scatter(p_initial(1), p_initial(2), "filled", 'd')
+    scatter(p_final(1), p_final(2), "filled", 'd')
+    % The waypoints
+    scatter(w(1, :), w(2, :), 300, "square", "MarkerEdgeColor", "red", 'LineWidth', 2);
+    % The positions
+    scatter(x(1, :),x(2, :), 'o', 'MarkerEdgeColor', "blue");
+    % The positions at tau - MIGHT BE WRONG BY 1
+    scatter(x(1, tau), x(2, tau), 200, 'o', 'MarkerEdgeColor', "magenta", 'LineWidth', 1.5);  
     title (sprintf('Pos for lambda = %f',lambda));
-    hold off
-%     figure()
-%     plot(u(1,:),u(2,:));
-%     title (sprintf('Control Signal for lambda = %f',lambda));
+    xlim(ax,[-1 31]);
+    ylim(ax,[-16 11]);
+    hold(ax,'off');
+end
+
+function [ norm_col ] = vec_sqr_sum(A)
+%vec_sqr_sum squares elementwise and sums all the columns for a matrix 
+    A= A.^2;
+    for i= 1: length(A(1,:))
+        aux=sum(A(:,i));
+        norm_col(i) = aux;
+    end
 end
